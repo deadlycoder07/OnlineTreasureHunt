545e16a1605c432e33642dd0bd9a41751b3dae8e
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-08-11 03:05:21 +0530

    static

--- oth/urls.py
+++ oth/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.contrib import auth
 from django.conf.urls.static import static
-app
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('treasure.urls')),
