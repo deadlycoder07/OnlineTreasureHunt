10e646a68f5284c2c49690c9e03e97e775132d5d
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-08-11 03:23:02 +0530

    auth

--- oth/urls.py
+++ oth/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.contrib import auth
+from django.contrib import auth 
 from django.conf.urls.static import static
-
+app_name=auth
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('treasure.urls')),
