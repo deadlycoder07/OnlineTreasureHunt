2ffeb8231efbc9f0dfa956c05c6352a7fec6e280
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-08-11 02:48:15 +0530

    static

--- oth/settings.py
+++ oth/settings.py
@@ -139,12 +139,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL ='/media/'
